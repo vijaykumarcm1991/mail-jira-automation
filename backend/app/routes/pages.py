ae6cc6e4d03bc397a01c85c44d96570b643e1855
vijaykumarcm1991 <vijaykumarcm20@gmail.com>
2026-04-26 12:37:37 +0530

    New Features Implemented

--- backend/app/routes/pages.py
+++ backend/app/routes/pages.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
+from app.config.settings import JIRA_BASE_URL
 
 router = APIRouter()
 templates = Jinja2Templates(directory="/frontend/templates")
@@ -17,7 +18,13 @@ def dashboard_page(request: Request):
     if "user" not in request.session:
         return RedirectResponse("/login")
 
-    return templates.TemplateResponse(request, "dashboard.html", {})
+    return templates.TemplateResponse(
+        request,
+        "dashboard.html",
+        {
+            "jira_base_url": JIRA_BASE_URL   # ✅ PASS TO UI
+        }
+    )
 
 @router.get("/mappings", response_class=HTMLResponse)
 def mappings_page(request: Request):
